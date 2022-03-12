397a81ccf0fc585467496bc5d50e7d3b5b6bebb6
leigp <907090585@qq.com>
2022-03-12 22:26:27 +0800

    完成chart6

--- src/components/Chart6.vue
+++ src/components/Chart6.vue
@@ -1,10 +1,38 @@
 <template>
-  <Border>
-  </Border>
+  <Echart :option="option" title="运单送达情况" root="echartF"/>
 </template>
 
 <script lang="ts" setup>
-import Border from './Border.vue'
+import Echart from "./Echart.vue";
+import * as echarts from 'echarts/core';
+import {ECOption} from "../type.d.ts/type";
+const px=(value:number)=>{
+  return (value/1920*(window.pagWidth))
+}
+
+const option:ECOption = {
+  grid:{
+    x:px(0),
+    y:px(0),
+    x2:px(0),
+    y2:px(0)
+  },
+  series: [
+    {
+      name: '车辆适配情况',
+      type: 'pie',
+      radius : '100%',//设置饼图大小
+      center: ['45%', '40%'],
+      radius: ['60%', '70%'],
+      avoidLabelOverlap: false,
+      labelLine: {
+        show: false
+      },
+      data: [{ value: 98 }, { value: 2 },],
+
+    }
+  ]
+};
 </script>
 
 <style lang="scss" scoped>
